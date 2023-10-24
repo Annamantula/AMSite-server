76996de14010ecf14ed62732bdd548da01e84ea6
Anna Mantula <ganna.mantula@gmail.com>
2023-10-24 07:42:31 -0500

    prt

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.post('/send-email', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
