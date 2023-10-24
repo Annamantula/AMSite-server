fb51544a09a46b23c95df1d22db56ffd06e5bd40
Anna Mantula <ganna.mantula@gmail.com>
2023-10-24 09:15:22 -0500

    v

--- server.js
+++ server.js
@@ -22,9 +22,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 app.use(helmet());
-app.get('/', (req, res, next) => {
-  res.json({msg:"API Working"});
-})
 
 
 // Create a Nodemailer transporter
@@ -36,23 +33,27 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+app.get('/', (req, res, next) => {
+  res.json({msg:"API Working"});
+})
 // Define a route to handle form submissions
 app.post('/send-email', (req, res) => {
-  const { name, email, message } = req.body;
-  const mailOptions = {
-    from: 'anya.mantula@hotmail.com',
-    to: 'anya.mantula@hotmail.com',
-    subject: `New contact from ${name}`,
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  };
+  res.json({msg:"send email path"});
+  // const { name, email, message } = req.body;
+  // const mailOptions = {
+  //   from: 'anya.mantula@hotmail.com',
+  //   to: 'anya.mantula@hotmail.com',
+  //   subject: `New contact from ${name}`,
+  //   text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  // };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).json({ success: false, message: 'Email could not be sent.' });
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).json({ success: true, message: 'Email sent successfully.'});
-    }
-  });
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //     console.log(error);
+  //     res.status(500).json({ success: false, message: 'Email could not be sent.' });
+  //   } else {
+  //     console.log('Email sent: ' + info.response);
+  //     res.status(200).json({ success: true, message: 'Email sent successfully.'});
+  //   }
+  // });
 });
