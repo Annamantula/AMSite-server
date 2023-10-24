e769a14f3c952f9b70990be9633970a4ef0b0817
Anna Mantula <ganna.mantula@gmail.com>
2023-10-24 08:58:16 -0500

    helmet

--- server.js
+++ server.js
@@ -3,15 +3,26 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 // Middleware to parse JSON and url-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+app.use(helmet());
+
 
 // Create a Nodemailer transporter
 const transporter = nodemailer.createTransport({
@@ -27,7 +38,7 @@ app.post('/send-email', (req, res) => {
   const { name, email, message } = req.body;
   const mailOptions = {
     from: 'anya.mantula@hotmail.com',
-    to: 'anya.mantula@hotmail.com', // Your email address
+    to: 'anya.mantula@hotmail.com',
     subject: `New contact from ${name}`,
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
@@ -41,8 +52,4 @@ app.post('/send-email', (req, res) => {
       res.status(200).json({ success: true, message: 'Email sent successfully.'});
     }
   });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
